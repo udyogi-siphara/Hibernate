79c19d4f4477f7d43931ff98dd420138743f1f58
udyogi-siphara <udyogisiphara22@gmail.com>
2022-06-14 03:13:54 -0700

    Item entity

---
+++ src/lk/ijse/superMarket/entity/Item.java
@@ -0,0 +1,12 @@
+/**
+ * @author : Udyogi Siphara
+ * Project Name: HibernateSuperMarket
+ * Date        : 6/14/2022
+ * Time        : 3:11 AM
+ */
+
+package lk.ijse.superMarket.entity;
+
+public class Item {
+
+}
